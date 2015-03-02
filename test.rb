ed268340cbe8ef7f2d5386796cb256bb78fe23c3
laniger <mail@joshavg.de>
2015-03-02 08:21:36 +0000

    custom date parsing, irc rfc does not tell the time format, so it becomes ugly

--- test.rb
+++ test.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'logger'
+require 'date'
 
 class Object
     def is_number?
@@ -54,8 +55,8 @@ class IRCClient
     end
 
     def accept(parsed, line)
-        if parsed[:cmd] == 'PING' then
-            write "PONG #{parsed[:value]}"
+        if parsed[:cmd] == "PING" then
+            write "PONG #{parsed[:payload]}"
         end
     end
 
@@ -65,12 +66,12 @@ class IRCClient
 
     private
     def parse_line(line)
-        regexp = /^:((?<nick>\S+)!(?<user>\S+)@)?(?<server>\S+)\s(?<cmd>\S+)\s((?<target>\S+)\s)?((?<target2>\S+)\s)?((:(?<payload>.+))|(?<params>.*))$/
+        regexp = /^:?((?<nick>\S+)!(?<user>\S+)@)?(?<server>\S+)\s(?<cmd>\S+)\s((?<target>\S+)\s)?((?<target2>\S+)\s)?((:(?<payload>.+))|(?<params>.*))$/
         matcher = regexp.match line
         # P[I|O]NG :payload
         pingMatcher = /(?<cmd>PING) :(?<payload>.*)/.match line
         
-        matcher == nil ? pingMatcher : matcher
+        pingMatcher != nil ? pingMatcher : matcher
     end
 end
 
@@ -84,7 +85,26 @@ class Leetwriter
     end
 
     def accept(parsed, line)
-        
+        if parsed[:cmd] == "391" then
+        	# contains something like 19:23:53 +01:00
+        	timevalue = parsed[:payload] == nil ? parsed[:params] : parsed[:payload]
+        	strtime = timevalue.split(" -- ").last
+        	@@logger.debug("got #{strtime.strip} as string time")
+        	
+        	# crappy irc rfc does not say which time format will be delivers,
+        	# so hard parsing will be the weapon of choice
+        	dateTimeParts = strtime.split(" ")
+        	datePart = dateTimeParts[0]
+        	#timePart = dateTimeParts[1]
+        	hour = datePart.split(":").first
+        	minute = datePart.split(":")[1]
+        	
+        	@@logger.debug("hour is #{hour}, minute is #{minute}")
+        	
+        	if hour == "13" and minute == "37" then
+        	    @client.broadcast "leet"
+    	    end
+        end
     end
     
     def connected
